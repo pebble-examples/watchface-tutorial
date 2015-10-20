4395ec8ccadb01f1347ea2a85dcef55ca02e84a5
Chris Lewis <Bonsitm@gmail.com>
2015-10-19 17:42:41 -0700

    Center time display

--- src/watchface-tutorial.c
+++ src/watchface-tutorial.c
@@ -1,7 +1,7 @@
 #include <pebble.h>
   
 #define KEY_TEMPERATURE 0
-#define KEY_CONDITIONS 1
+#define KEY_CONDITIONS  1
   
 static Window *s_main_window;
 static TextLayer *s_time_layer;
@@ -18,44 +18,60 @@ static void update_time() {
   time_t temp = time(NULL); 
   struct tm *tick_time = localtime(&temp);
  
-  // Create a long-lived buffer
-  static char buffer[] = "00:00";
- 
-  // Write the current hours and minutes into the buffer
-  if(clock_is_24h_style() == true) {
-    //Use 2h hour format
-    strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
-  } else {
-    //Use 12 hour format
-    strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
-  }
+  // Write the current hours and minutes into a buffer
+  static char s_buffer[8];
+  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ? "%H:%M" : "%I:%M", tick_time);
  
   // Display this time on the TextLayer
-  text_layer_set_text(s_time_layer, buffer);
+  text_layer_set_text(s_time_layer, s_buffer);
 }
  
 static void main_window_load(Window *window) {
-  //Create GBitmap, then set to created BitmapLayer
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+
+  // Create GBitmap
   s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
-  s_background_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
+
+  // Create BitmapLayer to display the GBitmap
+#if defined(PBL_SDK_2)
+  s_background_layer = bitmap_layer_create(bounds);
+#elif defined(PBL_SDK_3)
+  GRect bitmap_bounds = gbitmap_get_bounds(s_background_bitmap);
+
+  // Inset the window bounds to center the image
+  const GEdgeInsets bitmap_insets = GEdgeInsets(
+    (bounds.size.h - bitmap_bounds.size.h) / 2,
+    (bounds.size.w - bitmap_bounds.size.w) / 2);
+  s_background_layer = bitmap_layer_create(grect_inset(bounds, bitmap_insets));
+#endif
+
+  // Set the GBitmap to the BitmapLayer and add to the Window
   bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
-  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
+  layer_add_child(window_layer, bitmap_layer_get_layer(s_background_layer));
   
   // Create time TextLayer
+#if defined(PBL_SDK_2)
   s_time_layer = text_layer_create(GRect(5, 52, 139, 50));
+#elif defined(PBL_SDK_3)
+  const int text_height = 65;
+  const GEdgeInsets text_insets = GEdgeInsets((bounds.size.h - text_height) / 2, 0);
+
+  s_time_layer = text_layer_create(grect_inset(bounds, text_insets));
+#endif
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
   text_layer_set_text(s_time_layer, "00:00");
   
-  //Create GFont
+  // Create GFont
   s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_48));
  
-  //Apply to TextLayer
+  // Apply to TextLayer
   text_layer_set_font(s_time_layer, s_time_font);
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
  
   // Add it as a child layer to the Window's root layer
-  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
   
   // Create temperature Layer
   s_weather_layer = text_layer_create(GRect(0, 130, 144, 25));
@@ -67,20 +83,20 @@ static void main_window_load(Window *window) {
   // Create second custom font, apply it and add to Window
   s_weather_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_20));
   text_layer_set_font(s_weather_layer, s_weather_font);
-  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_weather_layer));
+  layer_add_child(window_layer, text_layer_get_layer(s_weather_layer));
   
   // Make sure the time is displayed from the start
   update_time();
 }
  
 static void main_window_unload(Window *window) {
-  //Unload GFont
+  // Unload GFont
   fonts_unload_custom_font(s_time_font);
   
-  //Destroy GBitmap
+  // Destroy GBitmap
   gbitmap_destroy(s_background_bitmap);
  
-  //Destroy BitmapLayer
+  // Destroy BitmapLayer
   bitmap_layer_destroy(s_background_layer);
   
   // Destroy TextLayer
@@ -156,6 +172,9 @@ static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
 static void init() {
   // Create main Window element and assign to pointer
   s_main_window = window_create();
+
+  // Set the background color
+  window_set_background_color(s_main_window, GColorBlack);
  
   // Set handlers to manage the elements inside the Window
   window_set_window_handlers(s_main_window, (WindowHandlers) {
