67cee73a1d50bb9c7917aadeac1188eb17e4fb97
Chris Lewis <Bonsitm@gmail.com>
2015-10-20 13:07:44 -0700

    Cleanup AppMessage code

--- src/watchface-tutorial.c
+++ src/watchface-tutorial.c
@@ -55,7 +55,6 @@ static void main_window_load(Window *window) {
 #elif defined(PBL_SDK_3)
   const int text_height = 65;
   const GEdgeInsets text_insets = GEdgeInsets((bounds.size.h - text_height) / 2, 0);
-
   s_time_layer = text_layer_create(grect_inset(bounds, text_insets));
 #endif
   text_layer_set_background_color(s_time_layer, GColorClear);
@@ -128,31 +127,19 @@ static void inbox_received_callback(DictionaryIterator *iterator, void *context)
   static char conditions_buffer[32];
   static char weather_layer_buffer[32];
   
-  // Read first item
-  Tuple *t = dict_read_first(iterator);
- 
-  // For all items
-  while(t != NULL) {
-    // Which key was received?
-    switch(t->key) {
-    case KEY_TEMPERATURE:
-      snprintf(temperature_buffer, sizeof(temperature_buffer), "%dC", (int)t->value->int32);
-      break;
-    case KEY_CONDITIONS:
-      snprintf(conditions_buffer, sizeof(conditions_buffer), "%s", t->value->cstring);
-      break;
-    default:
-      APP_LOG(APP_LOG_LEVEL_ERROR, "Key %d not recognized!", (int)t->key);
-      break;
-    }
- 
-    // Look for next item
-    t = dict_read_next(iterator);
+  // Read tuples for data
+  Tuple *temp_tuple = dict_find(iterator, KEY_TEMPERATURE);
+  Tuple *conditions_tuple = dict_find(iterator, KEY_CONDITIONS);
+
+  // If all data is available, use it
+  if(temp_tuple && conditions_tuple) {
+    snprintf(temperature_buffer, sizeof(temperature_buffer), "%dC", (int)temp_tuple->value->int32);
+    snprintf(conditions_buffer, sizeof(conditions_buffer), "%s", conditions_tuple->value->cstring);
+
+    // Assemble full string and display
+    snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s, %s", temperature_buffer, conditions_buffer);
+    text_layer_set_text(s_weather_layer, weather_layer_buffer);
   }
-  
-  // Assemble full string and display
-  snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s, %s", temperature_buffer, conditions_buffer);
-  text_layer_set_text(s_weather_layer, weather_layer_buffer);
 }
  
 static void inbox_dropped_callback(AppMessageResult reason, void *context) {
